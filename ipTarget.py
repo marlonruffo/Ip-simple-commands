a19f48003a7d7d5ef507b3c31437f8bd26ca3055
marlonruffo <ruffomarlon9@gmail.com>
2023-01-04 00:49:44 -0300

    colors + sleep

--- ipTarget.py
+++ ipTarget.py
@@ -9,6 +9,7 @@ import ipaddress
 import ping3
 import subprocess
 import nmap
+import time
 
 
 from datetime import datetime
@@ -19,33 +20,37 @@ colorama.init()
 
 
 while True: # loop until the user enters a valid IP
-  target = input(str("Target IP: "))
+  target = input(str(colorama.Fore.YELLOW+"Target IP: "))
 
   # check if the target matches the pattern
   if regex.fullmatch(target):
-    print("Valid IP")
+    print(colorama.Fore.GREEN+"Valid IP")
+    print(colorama.Fore.RED+"Aguarde...")
+    time.sleep(1.5)
     break
   else:
-    print("Invalid IP. Please try again.")
+    print(colorama.Fore.RED+"Invalid IP. Please try again.")
+print(colorama.Fore.YELLOW+"O que você deseja fazer com o IP: "+target+" ?")
 print(colorama.Fore.BLACK + "_"*82 + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLUE + "1 - Scan de portas" + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLUE + "2 - Verificar Classe do Ip" + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLUE + "3 - Ping no Ip" + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLUE + "4 - Traceroute no Ip" + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLUE + "5 - Hostname" + colorama.Style.RESET_ALL)
-print(colorama.Fore.BLUE + "6 - hosts disponíveis" + colorama.Style.RESET_ALL)
+print(colorama.Fore.BLUE + "6 - Qtd Hosts" + colorama.Style.RESET_ALL)
+print(colorama.Fore.RED + "7 - Sair" + colorama.Style.RESET_ALL)
 
-print(colorama.Fore.RED + "5 - Sair" + colorama.Style.RESET_ALL)
 print(colorama.Fore.BLACK + "_"*82 + colorama.Style.RESET_ALL)
 
-option = input("Escolha uma opção: ")
+option = input(colorama.Fore.RED +"Escolha uma opção: ")
 if option == "1":
-    start_port = int(input("Enter the start port to scan: "))
-    end_port = int(input("Enter the final port to scan: "))
+    print(colorama.Fore.RED + "You selected option 1: Scan ports")
+    start_port = int(input(colorama.Fore.BLUE+"Enter the start port to scan: "))
+    end_port = int(input(colorama.Fore.BLUE+"Enter the final port to scan: "))
     print("Scanning Target: " + target)
     start_time = datetime.now()
-    print("Time Started: " + str(datetime.now()))
-    print("Aguarde...")
+    print(colorama.Fore.BLUE+"Time Started: " + str(datetime.now()))
+    print(colorama.Fore.RED+"Aguarde...")
 
     open_ports = []
     closed_ports = []
@@ -71,35 +76,36 @@ if option == "1":
             t.join()
 
     except KeyboardInterrupt:  # if user press ctrl+c
-        print("Exiting Program !!!!")
+        print(colorama.Fore.BLUE+ "Exiting Program !!!!")
         sys.exit()
 
     except socket.error:  # if host is unreachable
-        print("Couldn't connect to server")
+        print(colorama.Fore.BLUE+ "Couldn't connect to server")
         sys.exit()
 
     # print the results
     open_ports.sort()
     closed_ports.sort()
-    print("Open ports are: ", open_ports)
+    print(colorama.Fore.BLUE+ "Open ports are: ", open_ports)
 
     end_time = datetime.now()  # store the end time
     total_time = end_time - start_time  # calculate the total time taken
     total_time_formatted = total_time.total_seconds()  # format the total time taken
-    print("Total time taken to read all the ports: {}".format(
-        total_time_formatted)+" seconds")  # print the total time taken
+    print(colorama.Fore.BLUE +"Total time taken to read all the ports: {}".format(
+        total_time_formatted)+colorama.Fore.BLUE +" seconds")  # print the total time taken
 
     while True:
         user_input = input(
-            "Enter 'see' to see closed ports or press 'n' to continue: ")
+            colorama.Fore.RED+"Enter 'see' to see closed ports or press 'n' to continue: ")
         if user_input == "see":
-            print("Closed ports are: ", closed_ports)
+            print(colorama.Fore.BLUE+"Closed ports are: ", closed_ports)
             break
         else:
-            print("Certo, você escolheu continuar e nao ver as portas fechadas")
+            print(colorama.Fore.BLUE+"Certo, você escolheu continuar e nao ver as portas fechadas")
             break
 
 if option == "2":
+    print(colorama.Fore.RED + "You selected option 2: Check IP class")
     ip = ipaddress.IPv4Address(target)
     if ip.is_private:
         # ip.packet transforms the ip into a binary format
@@ -122,6 +128,7 @@ if option == "2":
         print("Public address")
 
 if option == "3":
+    print(colorama.Fore.RED + "You selected option 3: Ping")
     ip = target
     response = ping3.ping(ip)
     if response == None:
@@ -130,7 +137,7 @@ if option == "3":
         print("Host is up")
         
 if option == "4": # Traceroute - trace the path to the target
-
+    print(colorama.Fore.RED + "You selected option 4: Traceroute")
     ip = target
     traceroute = subprocess.run(["tracert", ip], capture_output=True)
     try: # Try to decode the output as UTF-8
@@ -143,6 +150,7 @@ if option == "4": # Traceroute - trace the path to the target
     print(colorama.Fore.BLACK + "_"*82 + colorama.Style.RESET_ALL)
     print("Done")
 if option == "5":
+    print(colorama.Fore.RED + "You selected option 5: Reverse DNS Lookup")
     try:
         hostname = socket.gethostbyaddr(target)
         name = hostname[0]
@@ -156,11 +164,13 @@ if option == "5":
         print("IP: " + ip_string)
 
 if option == "6":
+    print(colorama.Fore.RED + "You selected option 6: Subnet Calculator")
     ip=target
-    prefix_lenght = int(input("Digite o prefixo: "))
+    prefix_lenght = int(input(colorama.Fore.BLUE+"Digite o prefixo: "))
     ip_network = ipaddress.IPv4Network((ip,prefix_lenght), strict=False)
     num_hosts = ip_network.num_addresses-2
     print("numero de hosts: " + str(num_hosts))
 
 if option == "7":
+    print(colorama.Fore.RED + "Você escolheu sair do programa :(")
     sys.exit()
